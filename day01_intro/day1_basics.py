942d16d3c20ffcd09f1ee6809fc139cf25032e5d
Nelson Rueda <nelsonrueda033@gmail.com>
2025-05-19 16:10:27 -0700

    Update day1_basics.py

--- day01_intro/day1_basics.py
+++ day01_intro/day1_basics.py
@@ -1,1 +1,10 @@
+# Greet user!
+print("Hello! Welcome to my SWE study log.!")
+name = input("What's your name? ")
+print(f"Great to meet you, {name}! Let's learn Python together.")
 
+# Just a simple addition code
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(f"The sum of {a} and {b} is {a + b}")
